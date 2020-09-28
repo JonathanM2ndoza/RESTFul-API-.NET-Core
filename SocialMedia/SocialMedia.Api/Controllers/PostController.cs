32ab124e5e48b85de1934d49238a9ef8e27ba93a
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2020-09-28 00:08:45 -0400

    Creation of Onion Architecture

---
+++ SocialMedia/SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories.Posts;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts() {
+            var post = new PostRepository().GetPosts();
+            return Ok(post);
+        }
+    }
+}
---
+++ SocialMedia/SocialMedia.Infrastructure/Repositories/Posts/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia.Domain.Models.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialMedia.Infrastructure.Repositories.Posts
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Descripcion{x}",
+                Date = DateTime.Now,
+                Image = $"http://test.images/{x}",
+                UserId = x * 2
+            });
+
+            return posts;
+        }
+    }
+}
